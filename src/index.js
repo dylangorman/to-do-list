e8c87e55e741ea1ac48d25fc13737ddeace162ca
Dylan Gorman <90197475+dylangorman@users.noreply.github.com>
2021-12-15 17:41:52 +0000

    second commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+// import App from './App';
+import ToDoList from "./ToDoList/ToDoList";
+ReactDOM.render(<ToDoList />, document.getElementById("root"));

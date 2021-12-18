1921d6956e64015fcb8c6f80ef4178b5d283cd8a
Dylan Gorman <90197475+dylangorman@users.noreply.github.com>
2021-12-18 22:04:48 +0000

    final commits?

--- src/ToDoList/ToDoList.js
+++ src/ToDoList/ToDoList.js
@@ -9,7 +9,11 @@ const Task = ({ task, index, completeTask, removeTask }) => {
       style={{ textDecoration: task.completed ? "line-through" : "" }}
     >
       {task.title}
-      <button style={{ background: "" }} onClick={() => completeTask(index)}>
+      <button
+        className="tick"
+        style={{ background: "" }}
+        onClick={() => completeTask(index)}
+      >
         ✓
       </button>
       <button
@@ -90,55 +94,3 @@ const ToDoList = () => {
   );
 };
 export default ToDoList;
-
-// import { useState } from "react";
-
-// const ToDoList = () => {
-//   const [list, setList] = useState([""]);
-//   const [userInput, setUserInput] = useState("");
-
-//   const updateInput = (event) => {
-//     setUserInput(event.target.value);
-//   };
-//   const addHandler = () => {
-//     const newArray = [...list];
-//     newArray.push(userInput);
-
-//     setList(newArray);
-//     setUserInput("");
-//   };
-//   const removeHandler = (index) => {
-//     const newArray = [...list];
-//     newArray.splice(index, 1);
-//     setList(newArray);
-//   };
-
-//   return (
-//     <div>
-//       <h1>To Do List</h1>
-//       <input type="text" value={userInput} onChange={updateInput} />
-//       <button onClick={addHandler}>add</button>
-//       <h2>{userInput}</h2>
-//       {list.map((item, index) => {
-//         return (
-//           <Card
-//             index={index}
-//             removeHandler={removeHandler}
-//             key={index}
-//             item={item}
-//           />
-//         );
-//       })}
-//     </div>
-//   );
-// };
-// const Card = (props) => {
-//   return (
-//     <div>
-//       <h2>{props.item}</h2>
-//       <button onClick={() => props.removeHandler(props.index)}>remove</button>
-//     </div>
-//   );
-// };
-
-// export default ToDoList;

eafd4f2be6e82a547d90ee1fa9752a667778ee55
Dylan Gorman <90197475+dylangorman@users.noreply.github.com>
2021-12-19 19:05:11 +0000

    Updates

---
+++ precache-manifest.912a5a9cef372168a099feac30beddfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "837713468d9fd3c0ddb0",
+    "url": "/to-do-list/static/css/main.22938a22.chunk.css"
+  },
+  {
+    "revision": "837713468d9fd3c0ddb0",
+    "url": "/to-do-list/static/js/main.18f47ae7.chunk.js"
+  },
+  {
+    "revision": "15bb3101340a9b514097",
+    "url": "/to-do-list/static/js/runtime~main.9900d293.js"
+  },
+  {
+    "revision": "6d53df18f34a5fca8851",
+    "url": "/to-do-list/static/js/2.200def56.chunk.js"
+  },
+  {
+    "revision": "5be167d0540be2d57b8d2c291abf6f03",
+    "url": "/to-do-list/index.html"
+  }
+];
